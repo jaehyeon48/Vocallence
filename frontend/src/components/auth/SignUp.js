84c49fc64b0d55dbbe0d67deb5fad6623722610b
jaehyeon48 <imjaehyeon48@gmail.com>
2020-07-23 13:12:04 +0900

    modified MainPage layout (added main-contaiver div)

--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+
+import { AuthContext } from '../../context/AuthContext';
 
 export default function SignUp() {
+  const [authInfo, setAuthInfo] = useContext(AuthContext);
+
+  if (authInfo.isAuthenticated) {
+    <Redirect to="/main" />
+  }
   return (
-    <div></div>
+    <div>
+
+    </div>
   );
 }
--- frontend/src/components/main_page/MainPage.js
+++ frontend/src/components/main_page/MainPage.js
@@ -10,7 +10,7 @@ import DeleteButton from './DeleteButton';
 
 export default function MainPage() {
   return (
-    <React.Fragment>
+    <div id="main-container">
       <WordCount />
       <WordMeaning />
       <ShowPrevWord />
@@ -18,6 +18,6 @@ export default function MainPage() {
       <Examples />
       <EditButton />
       <DeleteButton />
-    </React.Fragment>
+    </div>
   );
 }
