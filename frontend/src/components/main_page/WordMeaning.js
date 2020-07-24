1576d6ac20db4a5606e403f418778a0c3647176e
jaehyeon48 <imjaehyeon48@gmail.com>
2020-07-24 19:48:38 +0900

    added capitalize first character of a string logic

--- frontend/src/components/main_page/WordMeaning.js
+++ frontend/src/components/main_page/WordMeaning.js
@@ -6,9 +6,14 @@ export default function WordMeaning({
 }) {
   return (
     <div id="main-container__word">
-      <div id="main-container__word-name">{wordList.length > 0 && wordList[currentIndex].wordName}</div>
+      <div id="main-container__word-name">{wordList.length > 0 && CapitalizeFirstChar(wordList[currentIndex].wordName)}</div>
       <div id="main-container__word-class">{wordList.length > 0 && wordList[currentIndex].wordClass}</div>
       <div id="main-container__word-meaning">{wordList.length > 0 && wordList[currentIndex].wordMeaning}</div>
     </div>
   );
+}
+
+function CapitalizeFirstChar(word) {
+  let firstLetter = [...word][0];
+  return [firstLetter.toUpperCase(), ...word.slice(1)].join('');
 }
