f57eab5930259701fa2690d3e9c4246fc77663b5
jaehyeon48 <imjaehyeon48@gmail.com>
2020-07-24 19:21:30 +0900

    completed the skeleton of mainpage component

--- frontend/src/components/main_page/MainPage.js
+++ frontend/src/components/main_page/MainPage.js
@@ -1,32 +1,85 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 import WordCount from './WordCount';
 import WordMeaning from './WordMeaning';
 import ShowPrevWord from './ShowPrevWord';
 import ShowNextWord from './ShowNextWord';
 import Examples from './Examples';
-import EditButton from './EditButton';
-import DeleteButton from './DeleteButton';
+
+import './mainPage.css';
 
 import { AuthContext } from '../../context/AuthContext';
 
 export default function MainPage() {
   const [authInfo, setAuthInfo] = useContext(AuthContext);
+  const [wordList, setWordList] = useState([]);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      loadWordList();
+      setWordList(shuffleList(wordList));
+    }
+    return () => isMounted = false;
+  }, []);
+
+  async function loadWordList() {
+    try {
+      const response = await axios.get('/api/word');
+      if (response.status === 200) {
+        setWordList(response.data);
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
 
   if (!authInfo.isAuthenticated) {
     return <Redirect to="/login" />
   }
 
+  function handleShuffle() {
+    setCurrentWordIndex(0);
+    setWordList(shuffleList(wordList));
+  }
+
   return (
     <div id="main-container">
-      <WordCount />
-      <WordMeaning />
-      <ShowPrevWord />
-      <ShowNextWord />
-      <Examples />
-      <EditButton />
-      <DeleteButton />
+      <WordCount currentIndex={currentWordIndex} listLength={wordList.length} />
+      <WordMeaning wordList={wordList} currentIndex={currentWordIndex} />
+      <Examples wordList={wordList} currentIndex={currentWordIndex} />
+      <ShowPrevWord currentIndex={currentWordIndex} setWordIndex={setCurrentWordIndex} />
+      <ShowNextWord currentIndex={currentWordIndex} setWordIndex={setCurrentWordIndex} listLength={wordList.length} />
+      <div id="main-container__buttons-container">
+        <button id="main-container__edit-button" type="button">EDIT</button>
+        <button id="main-container__delete-button" type="button">DELETE</button>
+        <button id="main-container__shuffle-button" type="button" onClick={handleShuffle}>SHUFFLE!</button>
+      </div>
     </div>
   );
+}
+
+function shuffleList(wordList) {
+  let list = [...wordList];
+  let currentIndex = list.length
+  let temporaryValue
+  let randomIndex;
+
+  // While there remain elements to shuffle
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = list[currentIndex];
+    list[currentIndex] = list[randomIndex];
+    list[randomIndex] = temporaryValue;
+  }
+
+  return list;
 }
