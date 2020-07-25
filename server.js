9458b9f56470897570143c55afbfca3ace8d9cba
jaehyeon48 <imjaehyeon48@gmail.com>
2020-07-25 09:43:44 +0900

    initial app deployment

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.use(cookieParser());
 app.use('/api/auth', require('./routes/authRoute'));
 app.use('/api/word', require('./routes/wordRoute'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
